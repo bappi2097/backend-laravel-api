321ee1cbf9d4c9972ee64abf610b3e2cc6f0805b
Bappi Saha <bappi35-2097@diu.edu.bd>
2023-06-01 08:06:51 +0600

    done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        $response = [
+            'user' => $user
+        ];
+
+        return response($response, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,4 +23,11 @@ Route::middleware('auth:sanctum')->get('/user/{user}', function (User $user) {
 
 Route::post('/login', [AuthController::class, "login"]);
 Route::post('/register', [AuthController::class, "register"]);
-Route::post('/logout', [AuthController::class, "logout"])->middleware('auth:sanctum');
+
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout', [AuthController::class, "logout"]);
+    Route::get('/user/{user}', [UserController::class, "show"]);
+    Route::put('/user/{user}', [UserController::class, "update"]);
+
+});
